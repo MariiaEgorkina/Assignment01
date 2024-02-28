a8a3299a1f829ee09de3e037f2d5239ec5781713
Mariia Egorkina <mariiaegorkina@gmail.com>
2024-02-27 21:52:29 -0800

    init commit

---
+++ js/script.js
@@ -0,0 +1,158 @@
+//STEP 1
+let someMonth
+function theMonth
+let currentMonth
+let summerMonth
+let myLibraryFunction
+//The indentifiers are converted to Camel Case notation.
+
+//STEP 2
+Numeric: 54
+String: "Hello, World!"
+Boolean: true
+Null: null
+//There are examples numeric, string, Boolean, null literal expressions.
+
+//STEP 3
+let a = 5;
+let b = 8;
+let total = (a * b);
+console.log(total);
+
+let x = 5;
+let y = 38;
+let z = 7;
+let total = (x * y) + (y / z)
+console.log(total)
+//There are two examples of complex expressions.
+
+//STEP 4
+
+let firstName;
+let lastName;
+let city;
+let zipCode;
+let yourAge;
+let referralSource;
+let mayWeContactYou;
+//The indentifiers are declared with Camel Casing and Hungarian Natation.
+
+//STEP 5
+let firstName;
+let lastName;
+firstName = 'Mike';
+lastName = 'Smith';
+
+
+let firsName = 'Mike';
+let lastName = 'Smith';
+
+let firstName = 'Mike', lastName = 'Smith';
+//There are 3 ways for declaring.
+
+//STEP 6
+let result = 10 + '20';
+console.log(result);
+//Coerced result of number and string addition.
+
+//STEP 7
+let isSunny = true;
+let weather = isSunny + 'weather';
+console.log(weather);
+
+let rerult = false;
+console.log(result);
+//Coerced results Boolean and string, and number and Boolean.
+
+//STEP 8
+let someString = 'Who once said, "Only two things are infinite, the iniverse and human stupidity, and I'm not sure about the former."';
+console.log(someString);
+//Fix the string literal.
+
+//STEP 9
+let x = null;
+console.log(x);
+//null value
+
+let y;
+console.log(x);
+//an udifined value
+
+//STEP 10
+console.log(typeof "Hello");
+console.log(typeof 76);
+console.log(typeof true);
+console.log(typeof nullLiteral);
+console.log(typeof undefinedLiteral);
+//Using typeof operator on various literals
+
+//STEP 11
+let userName = "Mariia Egorkina";
+alert("Hello " + userName + , " welcome to the JavaScript class!");
+//an alert box with 2 concatination operators
+
+//STEP 12
+let userName = "Mariia Egorkina";
+userName = Jonh;
+alert("Hello " + userName + , " welcome to the JavaScript class!");
+//Declare a variable called name and set it equal to your name.
+
+//STEP 13
+let userName = "Mariia Egorkina";
+let course = "JavaScript";
+alert("Hello " + userName + , " welcome to the" + course + " class!");
+//Declare a variable called course
+
+//STEP 14
+let userName = "Mariia Egorkina";
+let course = "JavaScript";
+alert("Hello " + userName + " .\nWelcome to the" + course + " class!");
+//A line break is added 
+
+//Step 15
+let name = promt("What is your name?");
+let course = "JavaScript";
+alert("Hello " + name + " .\nWelcome to the" + course + " class!");
+//Promt that asks the user for their name.
+
+//STEP 16
+let name = promt("What is your name?");
+let course = promt("What class are you taken?");
+alert("Hello " + name + " .\nWelcome to the" + course + " class!");
+//Promt that asks what class are you taken
+
+//STEP 17
+let x = 10
+let y = 20
+console.log(x + y);
+//Sum of two numbers
+
+//STEP 18
+let x = 20;
+x += 20;
+console.log(x);
+//add and assign 20 to variable and display the result, that should be 40
+
+//STEP 19
+let x = 20;
+x *= 5;
+console.log(x);
+//multiply and assign 5. Result should bw 100
+
+//STEP 20
+let x = 20 % 3;
+x /= 1;
+console.log(x);
+//Declare a variable x and assign it a value than equals the remainder of 20 devided by 3. Divide and assign 1 to that variable. The result should be 2
+
+//STEP 21
+let x = 5;
+let y = 3;
+console.log(x > 3 && x !== y);
+//Using of set of Comparison and logic operators, white an application that evaluates to true.
+
+//STEP 22
+let x = 100;
+let y = 20;
+console.log(x < 3 || x === y);
+//Using of set of Comparison and logic operators, white an application that evaluates to false.
